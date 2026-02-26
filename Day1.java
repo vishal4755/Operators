aeec506715cd3e3df58d848bc577a5f3ad1fb2eb
Vishal <vishaltipe555@gmail.com>
2026-02-26 11:35:01 +0530

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,18 @@
+package com.tka.feb;
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		System.out.println("First One = 20");
+		int a = 20;
+		System.out.println("a + 20 " + "= " + (a + 50));
+		int b = 45;
+		System.out.println("a + b " + "= " + (a + b));
+		int c = a * b;
+		System.out.println("c / b" + " = " + (c / b));
+		int d = c % 4;
+		System.out.println("d - 2" + " = " + (d - 2));
+		System.out.println("d / c " + " = " + (d / c));
+	}
+
+}
