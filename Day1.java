5958a9e0331d92f58f74de4ab8bee2cad2616223
Vishal <vishaltipe555@gmail.com>
2026-02-26 11:43:34 +0530

    Fix output statement for division of d by c
    Corrected the output statement for 'd % c' to 'd / c'.

--- Day1.java
+++ Day1.java
@@ -12,7 +12,8 @@ public class Day1 {
 		System.out.println("c / b" + " = " + (c / b));
 		int d = c % 4;
 		System.out.println("d - 2" + " = " + (d - 2));
-		System.out.println("d / c " + " = " + (d / c));
+		System.out.println("d % c " + " = " + (d / c));
 	}
 
 }
+
